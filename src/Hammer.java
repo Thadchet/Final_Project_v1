09d9940771538f021a60dd3774df579e2e32dc2b
Wutipong Thabsuwan <wutipong1234@hotmail.com>
2018-11-24 16:03:41 +0700

    hit

--- src/Hammer.java
+++ src/Hammer.java
@@ -1,9 +1,70 @@
 
-public class Hammer {
-	private int damage;
+public class Hammer implements Hitable {
+	private int damage ;
+	private SumScore sumscore ;
+	private boolean reload;
+	private int health ;
+	final int maxhp = 10;
 	
-	public Hammer(int d) {
-		this.damage =d ;
+	public Hammer() {
+		this.damage = 1 ;
+		this.reload = false ;
+		this.health = 10 ;
+	}
+	
+	public int getDamage() {
+		return damage;
 	}
 
+	public void setDamage(int damage) {
+		this.damage = damage;
+	}
+
+	public SumScore getSumscore() {
+		return sumscore;
+	}
+
+	public void setSumscore(SumScore sumscore) {
+		this.sumscore = sumscore;
+	}
+
+	public boolean isReload() {
+		return reload;
+	}
+
+	public void setReload(boolean reload) {
+		this.reload = reload;
+	}
+
+	public int getHealth() {
+		return health;
+	}
+
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+
+	public void hit(Object o) {
+		if(o instanceof Rock) {
+			sumscore.addSocre(((Rock) o).getScore());
+		} 
+		///...............
+		this.reload = true ;
+	}
+	
+	public boolean isDead() {
+		return (health == 0);
+	}
+	
+	public void reloading() {
+		this.reload = false ; 
+	}
+	
+	public void takeDamage() {
+		
+	}
+	
+	
 }
---
+++ src/Hitable.java
@@ -0,0 +1,4 @@
+
+public interface Hitable {
+	void hit(Object o);
+}
