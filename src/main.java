f510bf06d9508ffd101099fcc7da4cd9bc2b6bdc
thadchet <sogood_boss@hotmail.com>
2018-11-24 14:19:23 +0700

    e

--- src/main.java
+++ src/main.java
@@ -1,12 +1,24 @@
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
+		BorderPane root = new BorderPane();
+		Scene scene = new Scene(root,800,600);
 		
+		primaryStage.setTitle("Battle Simulation");
+		primaryStage.setOnCloseRequest(e -> {
+			primaryStage.close();
+			System.exit(0);
+		});
+		primaryStage.setResizable(false);
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 
 	public static void main(String[] args) {
